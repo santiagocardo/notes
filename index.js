705ec92fc2b1fff3ab359ec0eadf9006a17fcf8e
Santiago Cardona <santiagocardo80@gmail.com>
2019-09-20 10:58:25 -0500

    Guardando las notas en las sesiones

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require('express')
+const cookieSession = require('cookie-session')
 const app = express()
-
-const logger = (req, res, next) => {
-  console.log('Nueva petición HTTP')
-  next()
-}
 
 app.set('view engine', 'pug')
 app.set('views', 'views')
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieSession({
+  secret: 'una_cadena_secreta',
+  maxAge: 24 * 60 * 60 * 1000
+}))
 app.use('/static', express.static('public'))
-app.use(logger)
 
 app.get('/', (req, res) => {
-  const name = req.query.name
-  const age = req.query.age
 
-  res.render('index', { name, age })
+  const notes = req.session.notes || []
+  
+  res.render('index', { notes })
 })
 
 app.get('/notes/new', (req, res) => {
@@ -24,13 +23,11 @@ app.get('/notes/new', (req, res) => {
 })
 
 app.post('/notes', (req, res) => {
-  console.log(req.body)
+  req.session.id = (req.session.id || 0) + 1
+  const id = req.session.id
+  req.session.notes = req.session.notes || []
+  req.session.notes.push({ id: id, title: req.body.title, body: req.body.body })
   res.redirect('/')
-})
-
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send('Algo salió mal')
 })
 
 app.listen(3000, () => console.log('Listening on port 3000'))
