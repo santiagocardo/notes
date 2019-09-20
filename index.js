2a81276e5cccb44c3e171486e635ffb14c80c31c
Santiago Cardona <santiagocardo80@gmail.com>
2019-09-20 10:07:23 -0500

    Rutas, vistas con Pug y Middlewares

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const express = require('express')
 const app = express()
 
+const logger = (req, res, next) => {
+  console.log('Nueva petición HTTP')
+  next()
+}
+
+app.set('view engine', 'pug')
+app.set('views', 'views')
+app.use(express.urlencoded({ extended: true }))
+app.use('/static', express.static('public'))
+app.use(logger)
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hola Mundo!</h1>')
+  const name = req.query.name
+  const age = req.query.age
+
+  res.render('index', { name, age })
+})
+
+app.get('/notes/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/notes', (req, res) => {
+  console.log(req.body)
+  res.redirect('/')
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Algo salió mal')
 })
 
 app.listen(3000, () => console.log('Listening on port 3000'))
